2f5391c391759054fdebd9b6f2f5c3140ea93aa6
Osmani Rosado-Falcon <osmanir@uclv.cu>
2021-08-07 22:10:54 -0400

    Update script to generate aliases files.

--- bash_aliases
+++
@@ -1,4 +0,0 @@
-
-alias judge1dc="docker-compose --project-directory $PWD/judge1 -f $PWD/docker-compose.yml"
-alias judge2dc="docker-compose --project-directory $PWD/judge2 -f $PWD/docker-compose.yml"
-alias judge3dc="docker-compose --project-directory $PWD/judge3 -f $PWD/docker-compose.yml"
--- setupenv.sh
+++ setupenv.sh
@@ -7,8 +7,13 @@ IMAGE_TAG=$IMAGE_TAG
 
 EOF
 
+ALIASES_FILE=bash_aliases
+echo "" > $ALIASES_FILE
+
 for i in {1..3}
 do
+    cat >>"$ALIASES_FILE" <<<"alias judge${i}dc='docker-compose --project-directory $PWD/judge${i} -f $PWD/docker-compose.yml'"
+
     NAME="judge$i"
     mkdir -p "$NAME"
 
