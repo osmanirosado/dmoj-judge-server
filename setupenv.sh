4854af6f99d5b71f7eefc5d0e81af3a13219b0a0
Osmani Rosado-Falcon <osmanir@uclv.cu>
2021-08-07 21:56:59 -0400

    Update script to generate env files for the judges.

--- setupenv.sh
+++ setupenv.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-cat > .env <<EOF
-# Environment variables for Docker Compose
+IMAGE_TAG=latest
 
-BRIDGE_ADDRESS=10.12.101.21
+cat > .env <<EOF
+IMAGE_TAG=$IMAGE_TAG
 
 EOF
 
+for i in {1..3}
+do
+    NAME="judge$i"
+    mkdir -p "$NAME"
+
+    cat > "$NAME/.env" <<EOF
+IMAGE_TAG=$IMAGE_TAG
+
+BRIDGE_ADDRESS=10.12.101.21
+
+JUDGE_NAME=main-judge-$i
+JUDGE_KEY=
+
+PROBLEMS_DIR=/mnt/dmoj/problems
+EOF
+
+done
