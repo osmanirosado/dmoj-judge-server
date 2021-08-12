0db3cf09ad47a8d76309e3d703dc838be4bc4216
Osmani Rosado-Falcon <osmanir@uclv.cu>
2021-08-12 18:01:50 -0400

    Introduce a - between judge prefix and the judge number in the directory name. The new directory name is judge-1 and the alias name is judge1dc. The difference between these names facilitate the shell expansion for the aliases.

--- setupenv.sh
+++ setupenv.sh
@@ -12,9 +12,9 @@ echo "" > $ALIASES_FILE
 
 for i in {1..3}
 do
-    cat >>"$ALIASES_FILE" <<<"alias judge${i}dc='docker-compose --project-directory $PWD/judge${i} -f $PWD/docker-compose.yml'"
+    NAME="judge-$i"
+    cat >>"$ALIASES_FILE" <<<"alias judge${i}dc='docker-compose --project-directory $PWD/$NAME -f $PWD/docker-compose.yml'"
 
-    NAME="judge$i"
     mkdir -p "$NAME"
 
     cat > "$NAME/.env" <<EOF
