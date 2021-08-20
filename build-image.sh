8dfdd445f3b48963a8707626d3eb14aa29963e23
Osmani Rosado-Falcon <osmanir@uclv.cu>
2021-08-20 16:20:19 -0400

    Delete unuseful code.

--- build-image.sh
+++ build-image.sh
@@ -9,6 +9,3 @@ docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:oldest"
 OUTPUT_FILE=build-image.out
 echo "[info] Running the docker compose build command in background. See the output in the '${OUTPUT_FILE}' file."
 nohup /usr/local/bin/docker-compose build --pull > ${OUTPUT_FILE} 2>&1 &
-
-# wait for message from nohup to be written in terminal
-sleep 2s
