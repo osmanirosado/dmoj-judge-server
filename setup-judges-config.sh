99618707605bbb6c59c00871fe5fdf2c3d561150
Osmani Rosado Falcon <osmanir@uclv.cu>
2025-09-27 09:21:21 -0400

    Project name prefix variable added to judges.conf file

--- setup-judges-config.sh
+++ setup-judges-config.sh
@@ -8,17 +8,18 @@ then
     echo "[info] Creating the file $CONF_FILE."
     cat > "$CONF_FILE" <<EOF
 JUDGE_NUMBER=3
-JUDGE_NAME_PREFIX='main-judge-'
+JUDGE_NAME_PREFIX='Arcadia-'
+PROJECT_NAME_PREFIX='arcadia-'
 
-BASE_IMAGE=dmoj/judge-tier3:latest
+BASE_IMAGE='dmoj/judge-tier3:latest'
 
-IMAGE_NAME=uclv-judge-tier3
-IMAGE_TAG_BUILD=latest
-IMAGE_TAG_ACTIVE=active
+IMAGE_NAME='uclv-judge-tier3'
+IMAGE_TAG_BUILD='latest'
+IMAGE_TAG_ACTIVE='active'
 
-BRIDGE_ADDRESS=10.12.101.21
+BRIDGE_ADDRESS='10.12.101.21'
 
-PROBLEMS_DIR=/var/opt/lib/dmoj/problems
+PROBLEMS_DIR='/var/opt/lib/dmoj/problems'
 EOF
 else
     echo "[warn] The file $CONF_FILE exits."
